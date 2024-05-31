e2308be78ca0932bafd02fd5217f540b6b4473bb
Higor Valuar <168334690+higorvaluar@users.noreply.github.com>
2024-05-31 11:38:21 -0300

    Grande atualização  - 31.05.2024
    - Foi adicionado o formulário, feito por Di Matheus e com algumas alterações visuais feitos por Iara e Higor.
    - Foi removido a caixa de pesquisa, pois não era necessário.
    - Foi removido o bloco de contato da página.
    - O botão Contato do menu foi substituído por 'Fale conosco' para levar a página do formulário.

---
+++ formulario/script.js
@@ -0,0 +1,9 @@
+function subscribe() {
+    const form = document.getElementById("contactForm");
+    form.addEventListener("submit", function(event) {
+        event.preventDefault();
+        document.getElementById("contato").innerHTML = "<p>Inscrição realizada com sucesso!</p>";
+    });
+}
+
+window.onload = subscribe;
